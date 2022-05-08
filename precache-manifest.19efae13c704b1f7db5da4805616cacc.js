d501782269ea91a78bc7b6bb7435447206a0ae98
dingyongya <yahya_dyy@163.com>
2022-05-08 20:42:18 +0800

    github部署

---
+++ precache-manifest.19efae13c704b1f7db5da4805616cacc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "717630799504e619cd06",
+    "url": "/vue-components/css/app.f32fcbc9.css"
+  },
+  {
+    "revision": "f0435306a6316e005c85",
+    "url": "/vue-components/css/chunk-vendors.6e2580b4.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vue-components/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4fbbbf27d5d446f71ce845865d383b5d",
+    "url": "/vue-components/index.html"
+  },
+  {
+    "revision": "28351ff66af01dd7222b",
+    "url": "/vue-components/js/about.12a2f4f5.js"
+  },
+  {
+    "revision": "717630799504e619cd06",
+    "url": "/vue-components/js/app.8c59c0e7.js"
+  },
+  {
+    "revision": "f0435306a6316e005c85",
+    "url": "/vue-components/js/chunk-vendors.72b2346e.js"
+  },
+  {
+    "revision": "c0653c92852596f88a2d7085120d609e",
+    "url": "/vue-components/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-components/robots.txt"
+  }
+]);
