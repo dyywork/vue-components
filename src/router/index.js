4ef7892d4b4c819b36c87c01ebde0fcdcfd34086
dingyongya <yahya_dyy@163.com>
2022-05-09 20:10:51 +0800

    fix: 样式调整

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,34 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Layout from "../Layout/Layout";
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: "/",
+    component: Layout,
+    children: [
+      {
+        path: '/',
+        name: 'Home',
+        component: Home
+      },
+      {
+        path: '/about',
+        name: 'About',
+        component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+      },
+      {
+        path: '/mg-search-form',
+        name: 'mgSearchForm',
+        component: () => import(/* webpackChunkName: "about" */ '@docs/mgSearchForm.md')
+      },
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/mg-search-form',
-    name: 'mgSearchForm',
-    component: () => import(/* webpackChunkName: "about" */ '#/mgSearchForm.md')
-  },
+
+
 ]
 
 const router = new VueRouter({
--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = {
     configureWebpack: {
       resolve: {
           alias: {
-              "#":  path.join(__dirname, 'docs')
+              "@docs":  path.join(__dirname, 'docs')
           }
       }
     },
