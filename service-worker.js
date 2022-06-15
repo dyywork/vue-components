4a8f3cb26ac7ff3b50a31f4afb5d9ccb4de979b7
dingyongya <yahya_dyy@163.com>
2022-06-15 16:51:35 +0800

    📝(component): searchForm
    searchForm 文档完善

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.e55992bfbdeea2fb75446a934dca764c.js"
+  "precache-manifest.e5fc3d5a792c3a36fcc72c5efa398dcc.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "dyy-vue-components"});
