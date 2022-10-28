eb4a63ccfdfb0f5ab8450d7c2edb739e3be761d2
dingyongya <yahya_dyy@163.com>
2022-10-28 14:12:39 +0800

    ✨(docs): docs
    文档首页更新

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 const container = require('markdown-it-container');
 const md = require('markdown-it');
 const WebpackBar = require('webpackbar')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 md({
     html: true
@@ -12,7 +13,16 @@ const {demo, title, table, anchor} = require('./src/config/mdPlugin')
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
     configureWebpack: {
-        plugins: [new WebpackBar()],
+        plugins: [
+            new WebpackBar(),
+            // new BundleAnalyzerPlugin(
+            //     {
+            //         openAnalyzer: false,
+            //         analyzerMode:'server', // 可以是 server、static、json、disabled。在server模式下，分析器将启动HTTP服务器来显示软件包报告。在“静态”模式下，会生成带有报告的单个HTML文件。在disabled模式下，你可以使用这个插件来将generateStatsFile设置为true来生成Webpack Stats JSON文件。
+            //         analyzerHost: '127.0.0.1', // 将在“服务器”模式下使用的端口启动HTTP服务器
+            //         analyzerPort: 9527
+            //     })
+        ],
         resolve: {
             alias: {
               "@docs":  path.join(__dirname, 'package/docs'),
